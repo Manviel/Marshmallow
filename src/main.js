cdfa2ad8391b33cf2a8e1d4cb57c68d2f90a9f15
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-01-06 20:45:46 +0200

    vue init vuetify/webpack-advanced

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+
+import App from './App'
+import router from './router'
+
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
