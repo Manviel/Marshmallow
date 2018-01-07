09a045f268f71c5ed2f7926baba0a71346aec05c
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-01-07 19:33:34 +0200

    Meetup Carousel on the Home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-    <h1>The Home Page</h1>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups">
+          Explore Meetups
+        </v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/new">
+          Organize Meetup
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="(meet) in meetups"
+            :src="meet.src" :key="meet.id">
+            <div class="title">
+              {{ meet.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  data () {
-    return {
+  export default {
+    data () {
+      return {
+        meetups: [
+          { src: 'http://ukamenterprises.com/wp-content/uploads/2015/07/Ni7326971.jpg', id: '01', title: 'Kyiv' },
+          { src: 'http://www.smileacadimos.gr/Images/Products/Netherlands-Amsterdam_413054785-20170921-114451.jpg', id: '02', title: 'Amsterdam' }
+        ]
+      }
     }
   }
-}
 </script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background: rgba(0, 0, 0, 0.5);
+    color: #fff;
+    font-size: 2rem;
+    padding: 10px;
+  }
+</style>
